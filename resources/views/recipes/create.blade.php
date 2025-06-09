b9f0b21095748386226f690aa9359f8f35f4c9f4
kawiwikiwi <kawiwikiwi@hotmail.com>
2025-06-09 11:36:19 +0100

    init

---
+++ resources/views/recipes/create.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.app :title="__('Create a recipe')">
+    <div>
+        
+    </div>
+</x-layouts.app>
---
+++ resources/views/recipes/index.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.app :title="__('My Recipes')">
+    <div>
+        
+    </div>
+</x-layouts.app>
