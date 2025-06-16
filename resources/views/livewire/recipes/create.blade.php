549c98650c2ea036562110f61ed7a662cae063c9
kawiwikiwi <kawiwikiwi@hotmail.com>
2025-06-16 16:33:11 +0100

    Add tailwindcss-animated dependency and update create recipe page layout with animations

--- resources/views/livewire/recipes/create.blade.php
+++ resources/views/livewire/recipes/create.blade.php
@@ -12,15 +12,18 @@ new #[Layout('components.layouts.app')] class extends Component {
 
     public function redirectToMyRecipes()
     {
+        Flux::modal('leave-page')->show();
+        Flux::modal('leave-page')->close();
+
         $this->redirect(route('recipes.index', absolute:false), navigate:true);
     }
 }; ?>
 
-<div class="flex items-start justify-center">
+<div class="flex items-start justify-center animate-flip-up">
     <div class="w-full border border-alternate-600/20 rounded-2xl max-h-[91vh] flex flex-col max-w-7xl pb-6 bg-white/80 dark:bg-zinc-900/50 backdrop-blur-sm h-screen">
-        <div class="bg-accent bg-clip-border border-top-none rounded-t-2xl shadow-xl pb-6 pt-10 px-14">
+        <div class="bg-accent bg-clip-border border-top-none rounded-t-2xl shadow-xl pb-6 pt-10 px-14 ">
             <flux:button icon="arrow-uturn-left" variant="ghost" class="absolute! left-0! top-0! py-5! px-7! bg-accent-700/50! hover:bg-[color-mix(in_oklab,_var(--color-accent-700),_transparent_10%)]! rounded-bl-none rounded-tr-none rounded-br-3xl rounded-tl-2xl!" size="sm" wire:click="redirectToMyRecipes">
-                        
+       
             </flux:button>
             <div class="items-center flex justify-between">
                 <div class="flex items-center justify-center ">
@@ -36,6 +39,8 @@ new #[Layout('components.layouts.app')] class extends Component {
                 </div>
             </div>
         </div>
+
+        <flux:modal name="leave-page"></flux:modal>
 
         <form class="mx-14 grid grid-cols-1 pt-10 gap-10 flex-1 overflow-y-scroll">
             <livewire:components.create-recipe-card title="Recipe Basics" type="basics"/>
